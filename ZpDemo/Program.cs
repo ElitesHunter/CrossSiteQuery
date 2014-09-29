9034fb16104d410145f5ac1c84fe2db844f5b311
Wang Yucai <Masterduner@Yeah.net>
2014-09-29 15:49:20 +0800

    新增了测试项目。

---
+++ ZpDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasterDuner.HHProjects.Csq.Highpincn.Trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
