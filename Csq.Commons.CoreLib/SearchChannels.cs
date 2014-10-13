262fa433fcae0fc8f12eef71a1eec4f17614ba52
Wang Yucai <Masterduner@Yeah.net>
2014-10-13 11:34:33 +0800

    新建的“搜索渠道”枚举类型：SearchChannels。

---
+++ Csq.Commons.CoreLib/SearchChannels.cs
@@ -0,0 +1,58 @@
+#region "SearchChannels"
+
+/*
+ * ####     Developer Name : Wang Yucai
+ * 
+ * ####     Development Tool : Microsoft VisualStudio 2010 Ultimate Edition
+ * 
+ * ####     Create Time : 2014-10-13 11:31:06
+ * 
+ * ####     Namespace : MasterDuner.Cooperations.Csq.Commons
+ * 
+ * ####     Type Name : SearchChannels
+ * 
+ * ####     Full Name : MasterDuner.Cooperations.Csq.Commons.SearchChannels
+ * 
+ * ####     Machine Name : GLCHQWYCWINW7
+ * 
+ * ####     UserName : GUOCOLAND/wangyucai
+ * 
+ * ####     CLR Version : 4.0.30319.18444
+ * 
+ * ####     Target Framework Version : 4.0
+ */
+
+#endregion
+
+using System;
+
+namespace MasterDuner.Cooperations.Csq.Commons
+{
+    /// <summary>
+    /// <para>MasterDuner.Cooperations.Csq.Commons.SearchChannels</para>
+    /// <para>
+    /// 搜索渠道枚举类型。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// <para>Target Framework Version : 4.0</para>
+    /// </remarks>
+    [Serializable()]
+    public enum SearchChannels
+    {
+    }
+}
+
+#region README
+
+/*
+ * ####     ©2014 Wang Yucai
+ * 
+ * ####     Contact me
+ *                  
+ *                  E-mail : Masterduner@Yeah.net
+ *                  Mobile : 13621384677
+ *                  QQ : 180261899
+ */
+
+#endregion
