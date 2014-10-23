6aeb815793845c9b117d8a5f698aaf3b31b49754
Wang Yucai <Masterduner@Yeah.net>
2014-10-23 10:11:07 +0800

    新增身份认证结果相关的实体类和枚举类型。

---
+++ Csq.Channels.HighpinCn/HPAuthenResult.cs
@@ -0,0 +1,101 @@
+#region ------------------------------------------------Header Description-------------------------------------------------------------
+
+/*
+ * .NET Programmer's Name : Wang Yucai
+ * 
+ * Development Tool : Microsoft Visual Studio 2010
+ * 
+ * Create Time : 2014-10-23 10:06:04
+ * 
+ * Machine Name : GLCHQWYCWINW7
+ * 
+ * Current User : GUOCOLAND/wangyucai
+ * 
+ * CLR Version : 4.0.30319.18444
+ * 
+ * Target Framework : This type is available for 4.0 and above versions of .NET Framework.
+ */
+
+#endregion
+
+using System;
+using System.Runtime.Serialization;
+using MasterDuner.Cooperations.Csq.Commons;
+
+namespace MasterDuner.Cooperations.Csq.Channels
+{
+    /// <summary>
+    /// <para>
+    /// 类型名称：<see cref="HPAuthenResult"/>
+    /// </para>
+    /// <para>
+    /// 命名空间：<see cref="MasterDuner.Cooperations.Csq.Channels"/>
+    /// </para>
+    /// <para>
+    /// 适用的.NET Framework版本：4.0
+    /// </para>
+    /// <para>
+    /// 智联卓聘网身份认证结果实体类。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// 此类型适用于4.0及其以上版本的.NET Framework。
+    /// <para>
+    /// 不可从此类继承。
+    /// </para>
+    /// </remarks>
+    [Serializable]
+    [DataContract]
+    public sealed class HPAuthenResult:AuthenticationResult
+    {
+        private HPAuthenStatus _authenStatus;
+
+        #region AuthenStatus
+        /// <summary>
+        /// 设置或获取智联卓聘网身份认证状态。
+        /// </summary>
+        [DataMember]
+        public HPAuthenStatus AuthenStatus
+        {
+            get { return _authenStatus; }
+            set
+            {
+                _authenStatus = value;
+                switch (value)
+                {
+                    case HPAuthenStatus.Completed: this.Status = PerformStatus.Completed; break;
+                    case HPAuthenStatus.WrongUserNameOrPassword: this.Status = PerformStatus.FailedCredentials; break;
+                    case HPAuthenStatus.WrongValidatingCode: this.Status = PerformStatus.FailedValidatingCode; break;
+                }
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// 初始化一个<see cref="HPAuthenResult" />对象实例。
+        /// </summary>
+        /// <remarks>
+        /// 不可从此类继承。
+        /// </remarks>
+        public HPAuthenResult()
+        { }
+
+        #endregion
+    }
+}
+
+#region Contact me
+
+/*
+ * E-mail Address : Masterduner@Yeah.net or Yucai.Wang-Public@Outlook.com
+ * 
+ * Mobile : +86 13621384677
+ * 
+ * QQ : 180261899
+ */
+
+#endregion
+
+/* Copyright © 2014 Wang Yucai. All rights reserved. */
---
+++ Csq.Channels.HighpinCn/HPAuthenStatus.cs
@@ -0,0 +1,77 @@
+#region ------------------------------------------------Header Description-------------------------------------------------------------
+
+/*
+ * .NET Programmer's Name : Wang Yucai
+ * 
+ * Development Tool : Microsoft Visual Studio 2010
+ * 
+ * Create Time : 2014-10-23 10:01:18
+ * 
+ * Machine Name : GLCHQWYCWINW7
+ * 
+ * Current User : GUOCOLAND/wangyucai
+ * 
+ * CLR Version : 4.0.30319.18444
+ * 
+ * Target Framework : This type is available for 4.0 and above versions of .NET Framework.
+ */
+
+#endregion
+
+using System;
+using System.Runtime.Serialization;
+
+namespace MasterDuner.Cooperations.Csq.Channels
+{
+    /// <summary>
+    /// <para>
+    /// 类型名称：<see cref="HPAuthenStatus"/>
+    /// </para>
+    /// <para>
+    /// 命名空间：<see cref="MasterDuner.Cooperations.Csq.Channels"/>
+    /// </para>
+    /// <para>
+    /// 适用的.NET Framework版本：4.0
+    /// </para>
+    /// <para>
+    /// 定义了智联卓聘网身份认证结果状态。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// 此类型适用于4.0及其以上版本的.NET Framework。
+    /// </remarks>
+    [Serializable]
+    [DataContract]
+    public enum HPAuthenStatus
+    {
+        /// <summary>
+        /// 错误的用户名密码。
+        /// </summary>
+        [EnumMember]
+        WrongUserNameOrPassword = 2,
+        /// <summary>
+        /// 错误的验证码。
+        /// </summary>
+        [EnumMember]
+        WrongValidatingCode = 14,
+        /// <summary>
+        /// 成功登录。
+        /// </summary>
+        [EnumMember]
+        Completed = 30
+    }
+}
+
+#region Contact me
+
+/*
+ * E-mail Address : Masterduner@Yeah.net or Yucai.Wang-Public@Outlook.com
+ * 
+ * Mobile : +86 13621384677
+ * 
+ * QQ : 180261899
+ */
+
+#endregion
+
+/* Copyright © 2014 Wang Yucai. All rights reserved. */
