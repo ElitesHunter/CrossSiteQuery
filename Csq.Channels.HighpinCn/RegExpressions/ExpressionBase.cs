01a6223c846c210a550d9c765c3a14b044d504fc
Wang Yucai <Masterduner@Yeah.net>
2014-10-23 10:36:25 +0800

    增加了正则表达式操作类。

---
+++ Csq.Channels.HighpinCn/RegExpressions/ExpressionBase.cs
@@ -0,0 +1,88 @@
+#region ------------------------------------------------Header Description-------------------------------------------------------------
+
+/*
+ * .NET Programmer's Name : Wang Yucai
+ * 
+ * Development Tool : Microsoft Visual Studio 2010
+ * 
+ * Create Time : 2014-10-23 10:25:10
+ * 
+ * Machine Name : GLCHQWYCWINW7
+ * 
+ * Current User : GUOCOLAND/wangyucai
+ * 
+ * CLR Version : 4.0.30319.18444
+ * 
+ * Target Framework : This type is available for 4.0 and above versions of .NET Framework.
+ */
+
+#endregion
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace MasterDuner.Cooperations.Csq.Channels.RegExpressions
+{
+    /// <summary>
+    /// <para>
+    /// 类型名称：<see cref="ExpressionBase"/>
+    /// </para>
+    /// <para>
+    /// 命名空间：<see cref="MasterDuner.Cooperations.Csq.Channels.RegExpressions"/>
+    /// </para>
+    /// <para>
+    /// 适用的.NET Framework版本：4.0
+    /// </para>
+    /// <para>
+    /// 正则表达式匹配基类。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// 此类型适用于4.0及其以上版本的.NET Framework。
+    /// </remarks>
+    public abstract class ExpressionBase
+    {
+        #region Expression
+        /// <summary>
+        /// 获取正则表达式。
+        /// </summary>
+        protected abstract string Expression { get; }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// 初始化一个<see cref="ExpressionBase" />对象实例。
+        /// </summary>
+        protected ExpressionBase()
+        { }
+
+        #endregion
+
+        #region IsMatch
+        /// <summary>
+        /// 文本内容是否与正则表达式匹配。
+        /// </summary>
+        /// <param name="s">需要匹配的文本内容。</param>
+        /// <returns><see cref="Boolean"/>值。</returns>
+        public virtual bool IsMatch(string s)
+        {
+            return Regex.IsMatch(s, this.Expression);
+        }
+        #endregion
+    }
+}
+
+#region Contact me
+
+/*
+ * E-mail Address : Masterduner@Yeah.net or Yucai.Wang-Public@Outlook.com
+ * 
+ * Mobile : +86 13621384677
+ * 
+ * QQ : 180261899
+ */
+
+#endregion
+
+/* Copyright © 2014 Wang Yucai. All rights reserved. */
---
+++ Csq.Channels.HighpinCn/RegExpressions/HPAuthenResponseTextExpression.cs
@@ -0,0 +1,83 @@
+#region ------------------------------------------------Header Description-------------------------------------------------------------
+
+/*
+ * .NET Programmer's Name : Wang Yucai
+ * 
+ * Development Tool : Microsoft Visual Studio 2010
+ * 
+ * Create Time : 2014-10-23 10:31:42
+ * 
+ * Machine Name : GLCHQWYCWINW7
+ * 
+ * Current User : GUOCOLAND/wangyucai
+ * 
+ * CLR Version : 4.0.30319.18444
+ * 
+ * Target Framework : This type is available for 4.0 and above versions of .NET Framework.
+ */
+
+#endregion
+
+
+namespace MasterDuner.Cooperations.Csq.Channels.RegExpressions
+{
+    /// <summary>
+    /// <para>
+    /// 类型名称：<see cref="HPAuthenResponseTextExpression"/>
+    /// </para>
+    /// <para>
+    /// 命名空间：<see cref="MasterDuner.Cooperations.Csq.Channels.RegExpressions"/>
+    /// </para>
+    /// <para>
+    /// 适用的.NET Framework版本：4.0
+    /// </para>
+    /// <para>
+    /// 用于匹配智联卓聘网身份认证响应文本的正则表达式。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// 此类型适用于4.0及其以上版本的.NET Framework。
+    /// <para>
+    /// 不可从此类继承。
+    /// </para>
+    /// </remarks>
+    public sealed class HPAuthenResponseTextExpression : ExpressionBase
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 初始化一个<see cref="HPAuthenResponseTextExpression" />对象实例。
+        /// </summary>
+        /// <remarks>
+        /// 不可从此类继承。
+        /// </remarks>
+        public HPAuthenResponseTextExpression()
+        { }
+
+        #endregion
+
+        #region Expression
+        /// <summary>
+        /// 获取正则表达式。
+        /// </summary>
+        protected override string Expression
+        {
+            get { return @"^[1-9]\d*$"; }
+        }
+        #endregion
+    }
+}
+
+#region Contact me
+
+/*
+ * E-mail Address : Masterduner@Yeah.net or Yucai.Wang-Public@Outlook.com
+ * 
+ * Mobile : +86 13621384677
+ * 
+ * QQ : 180261899
+ */
+
+#endregion
+
+/* Copyright © 2014 Wang Yucai. All rights reserved. */
