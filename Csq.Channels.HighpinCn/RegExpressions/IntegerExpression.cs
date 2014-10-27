abe8b2a47eab5c60425022e9ad8aabd1f0858de1
Wang Yucai <Masterduner@Yeah.net>
2014-10-27 15:34:35 +0800

    新增验证有符号的整型值正则表达式操作。

---
+++ Csq.Channels.HighpinCn/RegExpressions/IntegerExpression.cs
@@ -0,0 +1,84 @@
+#region ------------------------------------------------Header Description-------------------------------------------------------------
+
+/*
+ * .NET Programmer's Name : Wang Yucai
+ * 
+ * Development Tool : Microsoft Visual Studio 2010
+ * 
+ * Create Time : 2014-10-27 15:30:54
+ * 
+ * Machine Name : GLCHQWYCWINW7
+ * 
+ * Current User : GUOCOLAND/wangyucai
+ * 
+ * CLR Version : 4.0.30319.18444
+ * 
+ * Target Framework : This type is available for 4.0 and above versions of .NET Framework.
+ */
+
+#endregion
+
+using System;
+
+namespace MasterDuner.Cooperations.Csq.Channels.RegExpressions
+{
+    /// <summary>
+    /// <para>
+    /// 类型名称：<see cref="IntegerExpression"/>
+    /// </para>
+    /// <para>
+    /// 命名空间：<see cref="MasterDuner.Cooperations.Csq.Channels.RegExpressions"/>
+    /// </para>
+    /// <para>
+    /// 适用的.NET Framework版本：4.0
+    /// </para>
+    /// <para>
+    /// 有符号的整型值验证正则表达式。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// 此类型适用于4.0及其以上版本的.NET Framework。
+    /// <para>
+    /// 不可从此类继承。
+    /// </para>
+    /// </remarks>
+    public sealed class IntegerExpression : ExpressionBase
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 初始化一个<see cref="IntegerExpression" />对象实例。
+        /// </summary>
+        /// <remarks>
+        /// 不可从此类继承。
+        /// </remarks>
+        public IntegerExpression()
+        { }
+
+        #endregion
+
+        #region Expression
+        /// <summary>
+        /// 获取正则表达式。
+        /// </summary>
+        protected override string Expression
+        {
+            get { return @"^-?\d+$"; }
+        }
+        #endregion
+    }
+}
+
+#region Contact me
+
+/*
+ * E-mail Address : Masterduner@Yeah.net or Yucai.Wang-Public@Outlook.com
+ * 
+ * Mobile : +86 13621384677
+ * 
+ * QQ : 180261899
+ */
+
+#endregion
+
+/* Copyright © 2014 Wang Yucai. All rights reserved. */
